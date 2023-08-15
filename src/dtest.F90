5a0b7f4c7f22b593123dcfaa8ad5fedea0e6503b
Vedran Novakovic <venovako@venovako.eu>
2023-08-15 19:32:18 +0200

    further updates

--- src/dtest.F90
+++ src/dtest.F90
@@ -1,6 +1,8 @@
 PROGRAM DTEST
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_double, c_long_double
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL128
   IMPLICIT NONE
+  REAL(c_double), PARAMETER :: HALF = 0.5_c_double
 9 FORMAT(A,ES25.17E3)
 CONTAINS
 #include "orfile.F90"
--- src/xtest.F90
+++ src/xtest.F90
@@ -1,6 +1,8 @@
 PROGRAM XTEST
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL128
   IMPLICIT NONE
+  REAL(c_long_double), PARAMETER :: HALF = 0.5_c_long_double
 9 FORMAT(A,ES30.21E4)
 CONTAINS
 #include "orfile.F90"
