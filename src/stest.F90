bf1a581ab216bc5fa9b85a0e879fe98d52ed2efd
Vedran Novakovic <venovako@venovako.eu>
2023-08-17 00:01:08 +0200

    further updates

--- src/stest.F90
+++ src/stest.F90
@@ -2,58 +2,124 @@ PROGRAM STEST
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_float
   USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL128
   IMPLICIT NONE
+  INTEGER, PARAMETER :: CLAL = 256
+  REAL(REAL128), PARAMETER :: QZERO = 0.0_REAL128, QONE = 1.0_REAL128
+  CHARACTER(LEN=CLAL) :: CLA
   REAL(c_float), PARAMETER :: HALF = 0.5_c_float
   REAL(c_float), TARGET :: A, B, C, RT1, RT2, CS1, SN1
   REAL(REAL128), TARGET :: QA, QB, QC, QRT1, QRT2, QCS1, QSN1
-  REAL(REAL128) :: D, REC, RES
-  INTEGER :: U
+  REAL(REAL128) :: QJD, QLD, QREC, QRES, MJD, XJD, MLD, XLD, MREC, XREC, MRES, XRES, E_2
+  INTEGER :: I, N, U, INFO
   EXTERNAL :: SJIEV2, SLAEV2, QJAEV2
+
+  I = CLAL
+  CALL GET_COMMAND_ARGUMENT(0, CLA, I, INFO)
+  IF (INFO .NE. 0) STOP 'argv[0]'
+  CLA = TRIM(CLA)//' N'
+  IF (COMMAND_ARGUMENT_COUNT() .NE. 1) STOP CLA
+  I = CLAL
+  CALL GET_COMMAND_ARGUMENT(1, CLA, I, INFO)
+  IF (INFO .NE. 0) STOP 'argv[1]'
+  READ (CLA,*) N
+  IF (N .LE. 0) STOP 'N < 0'
   U = ORFILE()
-  A = SRSAFE(U)
-  B = SRSAFE(U)
-  C = SRSAFE(U)
+  E_2 = QZERO
+  E_2 = QONE / E_2
+  MJD = E_2
+  MLD = E_2
+  MREC = E_2
+  MRES = E_2
+  E_2 = -E_2
+  XJD = E_2
+  XLD = E_2
+  XREC = E_2
+  XRES = E_2
+  E_2 = EPSILON(HALF) * HALF
+  I = 1
+  DO WHILE (I .LE. N)
+     A = SRSAFE(U)
+     B = SRSAFE(U)
+     C = SRSAFE(U)
+#ifndef NDEBUG
+     WRITE (*,9,ADVANCE='NO') 'A(1,:):', A
+     WRITE (*,9) ',', B
+     WRITE (*,9,ADVANCE='NO') 'A(2,:):', B
+     WRITE (*,9) ',', C
+#endif
+     INFO = 0
+     CALL SJIEV2(A, B, C, RT1, RT2, CS1, SN1, INFO)
+     IF (INFO .NE. 0) THEN
+#ifndef NDEBUG
+        WRITE (*,'(A,I2)') 'SJIEV2=', INFO
+#endif
+        CYCLE
+     END IF
+#ifndef NDEBUG
+     WRITE (*,9) ' CS1=', CS1
+     WRITE (*,9) ' SN1=', SN1
+     WRITE (*,9) ' RT1=', RT1
+     WRITE (*,9) ' RT2=', RT2
+#endif
+     QA = A
+     QB = B
+     QC = C
+     CALL QJAEV2(QA, QB, QC, QRT1, QRT2, QCS1, QSN1)
+#ifndef NDEBUG
+     WRITE (*,9) 'QCS1=', QCS1
+     WRITE (*,9) 'QSN1=', QSN1
+     WRITE (*,9) 'QRT1=', QRT1
+     WRITE (*,9) 'QRT2=', QRT2
+#endif
+     QREC = CS1
+     QRES = QCS1
+     QREC = QRE(QREC, QRES, E_2)
+#ifndef NDEBUG
+     WRITE (*,9) 'QREC=', QREC
+#endif
+     IF (QREC .LT. MREC) MREC = QREC
+     IF (QREC .GT. XREC) XREC = QREC
+     QREC = SN1
+     QRES = QSN1
+     QRES = QRE(QREC, QRES, E_2)
+#ifndef NDEBUG
+     WRITE (*,9) 'QRES=', QRES
+#endif
+     IF (QRES .LT. MRES) MRES = QRES
+     IF (QRES .GT. XRES) XRES = QRES
+     QREC = CS1
+     QRES = SN1
+     QJD = QDETM1(QREC, QRES, E_2)
+#ifndef NDEBUG
+     WRITE (*,9) ' QJD=', QJD
+#endif
+     IF (QJD .LT. MJD) MJD = QJD
+     IF (QJD .GT. XJD) XJD = QJD
+     CALL SLAEV2(A, B, C, RT1, RT2, CS1, SN1)
+#ifndef NDEBUG
+     WRITE (*,9) 'LCS1=', CS1
+     WRITE (*,9) 'LSN1=', SN1
+     WRITE (*,9) 'LRT1=', RT1
+     WRITE (*,9) 'LRT2=', RT2
+#endif
+     QREC = CS1
+     QRES = SN1
+     QLD = QDETM1(QREC, QRES, E_2)
+#ifndef NDEBUG
+     WRITE (*,9) ' QLD=', QLD
+#endif
+     IF (QLD .LT. MLD) MLD = QLD
+     IF (QLD .GT. XLD) XLD = QLD
+     I = I + 1
+  END DO
   CLOSE(U)
-  WRITE (*,9,ADVANCE='NO') 'A(1,:):', A
-  WRITE (*,9) ',', B
-  WRITE (*,9,ADVANCE='NO') 'A(2,:):', B
-  WRITE (*,9) ',', C
-  U = 0
-  CALL SJIEV2(A, B, C, RT1, RT2, CS1, SN1, U)
-  IF (U .NE. 0) WRITE (*,'(A,I2)') 'SJIEV2=', U
-  WRITE (*,9) ' CS1=', CS1
-  WRITE (*,9) ' SN1=', SN1
-  WRITE (*,9) ' RT1=', RT1
-  WRITE (*,9) ' RT2=', RT2
-  REC = CS1
-  RES = SN1
-  D = QDETM1(REC, RES)
-  WRITE (*,9) 'QDET=', D
-  QA = A
-  QB = B
-  QC = C
-  CALL QJAEV2(QA, QB, QC, QRT1, QRT2, QCS1, QSN1)
-  WRITE (*,9) 'QCS1=', QCS1
-  WRITE (*,9) 'QSN1=', QSN1
-  WRITE (*,9) 'QRT1=', QRT1
-  WRITE (*,9) 'QRT2=', QRT2
-  D = EPSILON(HALF) * HALF
-  REC = CS1
-  RES = QCS1
-  REC = QRE(REC, RES, D)
-  WRITE (*,9) 'QREC=', REC
-  REC = SN1
-  RES = QSN1
-  RES = QRE(REC, RES, D)
-  WRITE (*,9) 'QRES=', RES
-  CALL SLAEV2(A, B, C, RT1, RT2, CS1, SN1)
-  WRITE (*,9) 'LCS1=', CS1
-  WRITE (*,9) 'LSN1=', SN1
-  WRITE (*,9) 'LRT1=', RT1
-  WRITE (*,9) 'LRT2=', RT2
-  REC = CS1
-  RES = SN1
-  D = QDETM1(REC, RES)
-  WRITE (*,9) 'QDET=', D
+  WRITE (*,9) 'SJAEV2:min((det(U)-1)/ε)=', MJD
+  WRITE (*,9) 'SJAEV2:max((det(U)-1)/ε)=', XJD
+  WRITE (*,9) 'SLAEV2:min((det(U)-1)/ε)=', MLD
+  WRITE (*,9) 'SLAEV2:max((det(U)-1)/ε)=', XLD
+  WRITE (*,9) '     min(relerr(cosφ)/ε)=', MREC
+  WRITE (*,9) '     max(relerr(cosφ)/ε)=', XREC
+  WRITE (*,9) '     min(relerr(sinφ)/ε)=', MRES
+  WRITE (*,9) '     max(relerr(sinφ)/ε)=', XRES
 9 FORMAT(A,ES16.9E2)
 CONTAINS
 #include "orfile.F90"
