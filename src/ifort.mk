bf02716c8b308e4c0edb565407034c9498851b6c
Vedran Novakovic <venovako@venovako.eu>
2023-08-17 03:29:34 +0200

    further updates

--- src/ifort.mk
+++ src/ifort.mk
@@ -8,7 +8,7 @@ FCFLAGS=-O0 -g
 endif # ?NDEBUG
 FCFLAGS += -xHost -fPIC -fexceptions -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -standard-semantics -recursive -traceback -vec-threshold0 -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qsimd-honor-fp-model -qsimd-serialize-fp-reduction
 ifdef NDEBUG
-FCFLAGS += -qopt-report=5 -inline-level=2
+FCFLAGS += -qopt-report=5 -diag-disable=10397 -inline-level=2
 else # !NDEBUG
 FCFLAGS += -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -check all -fp-stack-check
 endif # ?NDEBUG
