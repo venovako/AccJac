d30d1a202bfc52c26e9b4f108f773029384d94df
Vedran Novakovic <venovako@venovako.eu>
2024-09-17 21:36:31 +0200

    further updates

--- src/ifort.mk
+++ src/ifort.mk
@@ -3,7 +3,7 @@ ARFLAGS=-qnoipo -lib rsv
 FC=$(COMPILER_PREFIX)ifort$(COMPILER_SUFFIX)
 CC=$(COMPILER_PREFIX)icc$(COMPILER_SUFFIX)
 FCFLAGS=-fpp
-CFLAGS=-std=gnu2x
+CFLAGS=-std=gnu18
 ifdef NDEBUG
 FCFLAGS += -O$(NDEBUG)
 CFLAGS += -O$(NDEBUG)
@@ -15,8 +15,8 @@ ifndef CPU
 CPU=Host
 # COMMON-AVX512 for KNLs
 endif # !CPU
-FCFLAGS += -x$(CPU) -fPIC -fexceptions -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -recursive -standard-semantics -traceback -vec-threshold0
-CFLAGS += -x$(CPU) -fPIC -fexceptions -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -traceback -vec-threshold0
+FCFLAGS += -x$(CPU) -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -recursive -standard-semantics -traceback -vec-threshold0
+CFLAGS += -x$(CPU) -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -traceback -vec-threshold0
 ifdef NDEBUG
 FCFLAGS += -fno-math-errno -qopt-report=5 -diag-disable=10397
 CFLAGS += -fno-math-errno -qopt-report=5 -diag-disable=10397
