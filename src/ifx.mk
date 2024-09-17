a72a2e78b02c5d030abba54b93cb8a827183cf9a
Vedran Novakovic <venovako@venovako.eu>
2024-09-17 20:56:46 +0200

    further updates

--- src/ifx.mk
+++ src/ifx.mk
@@ -18,8 +18,12 @@ endif # !CPU
 FCFLAGS += -x$(CPU) -mprefer-vector-width=512 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fp-model=precise -fp-speculation=safe -fimf-precision=high -fma -fprotect-parens -no-ftz -recursive -standard-semantics -traceback -vec-threshold0
 CFLAGS += -x$(CPU) -mprefer-vector-width=512 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fp-model=precise -fp-speculation=safe -fimf-precision=high -fma -fprotect-parens -no-ftz -traceback -vec-threshold0
 ifdef NDEBUG
-FCFLAGS += -fno-math-errno -inline-level=2 -qopt-report=3
-CFLAGS += -fno-math-errno -inline-level=2 -qopt-report=3
+FCFLAGS += -fno-math-errno -qopt-report=3
+CFLAGS += -fno-math-errno -qopt-report=3
+ifndef PROFILE
+FCFLAGS += -inline-level=2
+CFLAGS += -inline-level=2
+endif # !PROFILE
 else # !NDEBUG
 FCFLAGS += -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -check all
 CFLAGS += -debug extended -debug inline-debug-info -debug pubnames
