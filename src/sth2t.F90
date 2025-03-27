b99ddcb85e5b23fdcfd8a0cad56f273c35680509
Vedran Novakovic <venovako@venovako.eu>
2025-03-27 04:43:33 +0100

    further updates

--- src/sth2t.F90
+++ src/sth2t.F90
@@ -13,9 +13,9 @@ PROGRAM STH2T
   REAL(KIND=REAL128), PARAMETER :: QZERO = 0.0_REAL128, QONE = 1.0_REAL128
   REAL(KIND=REAL32), PARAMETER :: ZERO = 0.0_REAL32, ONE = 1.0_REAL32
   REAL(KIND=REAL32), PARAMETER :: CUTOFF = 40.0_REAL32 / 41.0_REAL32, DEPS = EPSILON(ZERO) / 2
-  INTEGER, PARAMETER :: ETH = 1, ECH = 2, ESH = 3, ARE = 1, MRE = 2
+  INTEGER, PARAMETER :: ETH = 1, ECH = 2, ESH = 3, STH = 4, SCH = 5, SSH = 6, ARE = 1, MRE = 2
   CHARACTER(LEN=256) :: CLA
-  REAL(KIND=REAL128) :: Q(2,3), QD, QT, QC, QS, QE
+  REAL(KIND=REAL128) :: Q(2,6), QD, QT, QC, QS, QE
   REAL(KIND=REAL32) :: D, T, C, S
   INTEGER, ALLOCATABLE :: ISEED(:)
   !DIR$ ATTRIBUTES ALIGN: 64:: ISEED
@@ -70,28 +70,46 @@ PROGRAM STH2T
         ISEED(1) = ISEED(1) + 1
         IF (N .LT. 0) GOTO 1
      END IF
+     ! "enhanced" formulas
      T = D / (ONE + SQRT(IEEE_FMA(-D, D, ONE)))
      C = CR_RSQRT(IEEE_FMA(-T, T, ONE))
      S = C * T
+     ! error check
      QD = D
      QT = QD / (QONE + SQRT(IEEE_FMA(-QD, QD, QONE)))
-     QS = SQRT(IEEE_FMA(-QT, QT, QONE))
-     QC = QONE / QS
-     QS = QT / QS
+     QC = QONE / SQRT(IEEE_FMA(-QT, QT, QONE))
+     QS = QC * QT
      QD = I - 1
      QD = QD / ABS(N)
      QE = T
      QE = ABS(QT - QE) / QT
-     Q(ARE,ETH) = Q(ARE,ETH) * QD + QE / I
+     Q(ARE,ETH) = IEEE_FMA(Q(ARE,ETH), QD, QE / I)
      Q(MRE,ETH) = MAX(Q(MRE,ETH), QE)
      QE = C
      QE = ABS(QC - QE) / QC
-     Q(ARE,ECH) = Q(ARE,ECH) * QD + QE / I
+     Q(ARE,ECH) = IEEE_FMA(Q(ARE,ECH), QD, QE / I)
      Q(MRE,ECH) = MAX(Q(MRE,ECH), QE)
      QE = S
      QE = ABS(QS - QE) / QS
-     Q(ARE,ESH) = Q(ARE,ESH) * QD + QE / I
+     Q(ARE,ESH) = IEEE_FMA(Q(ARE,ESH), QD, QE / I)
      Q(MRE,ESH) = MAX(Q(MRE,ESH), QE)
+     ! "standard" formulas
+     T = D / (ONE + SQRT((ONE - D) * (ONE + D)))
+     C = ONE / SQRT((ONE - T) * (ONE + T))
+     S = C * T
+     ! error check
+     QE = T
+     QE = ABS(QT - QE) / QT
+     Q(ARE,STH) = IEEE_FMA(Q(ARE,STH), QD, QE / I)
+     Q(MRE,STH) = MAX(Q(MRE,STH), QE)
+     QE = C
+     QE = ABS(QC - QE) / QC
+     Q(ARE,SCH) = IEEE_FMA(Q(ARE,SCH), QD, QE / I)
+     Q(MRE,SCH) = MAX(Q(MRE,SCH), QE)
+     QE = S
+     QE = ABS(QS - QE) / QS
+     Q(ARE,SSH) = IEEE_FMA(Q(ARE,SSH), QD, QE / I)
+     Q(MRE,SSH) = MAX(Q(MRE,SSH), QE)
   END DO
 #ifndef NDEBUG
   IF (N .LT. 0) WRITE (ERROR_UNIT,'(A,I11)') 'Skipped:', ISEED(1)
@@ -100,11 +118,7 @@ PROGRAM STH2T
   ! relative errors in the terms of \epsilon
   QE = DEPS
   Q = Q / DEPS
-#ifndef NDEBUG
-  WRITE (OUTPUT_UNIT,'(A)') '"EXP(TH(2φ))", "N", "AVG(ρ(TH))/ε", "MAX(ρ(TH))/ε", "AVG(ρ(CH))/ε", "MAX(ρ(CH))/ε", "AVG(ρ(SH))/ε", "MAX(ρ(SH))/ε"'
-#endif
-  WRITE (OUTPUT_UNIT,'(I3,A,I11,6(A,ES16.9E2))') NEXP, ',', ABS(N), &
-       ',', Q(ARE,ETH), ',', Q(MRE,ETH), &
-       ',', Q(ARE,ECH), ',', Q(MRE,ECH), &
-       ',', Q(ARE,ESH), ',', Q(MRE,ESH)
+  WRITE (OUTPUT_UNIT,'(I3,A,I11,12(A,ES16.9E2))') NEXP, ',', ABS(N), ',', &
+       Q(ARE,ETH), ',', Q(MRE,ETH), ',', Q(ARE,ECH), ',', Q(MRE,ECH), ',', Q(ARE,ESH), ',', Q(MRE,ESH), ',', &
+       Q(ARE,STH), ',', Q(MRE,STH), ',', Q(ARE,SCH), ',', Q(MRE,SCH), ',', Q(ARE,SSH), ',', Q(MRE,SSH)
 END PROGRAM STH2T
