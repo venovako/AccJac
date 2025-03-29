491afe55e1bc31420a7b467f45b843e900689336
Vedran Novakovic <venovako@venovako.eu>
2025-03-29 14:29:29 +0100

    further updates

--- src/sth2t.F90
+++ src/sth2t.F90
@@ -1,6 +1,6 @@
 PROGRAM STH2T
   USE, INTRINSIC :: IEEE_ARITHMETIC, ONLY: IEEE_FMA
-  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: ERROR_UNIT, OUTPUT_UNIT, REAL32, REAL128
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: ERROR_UNIT, OUTPUT_UNIT, INT32, REAL32, REAL128
   IMPLICIT NONE
   INTERFACE
      PURE FUNCTION CR_RSQRT(X) BIND(C,NAME='cr_rsqrtf')
@@ -16,60 +16,23 @@ PROGRAM STH2T
   INTEGER, PARAMETER :: ETH = 1, ECH = 2, ESH = 3, STH = 4, SCH = 5, SSH = 6, ARE = 1, MRE = 2
   CHARACTER(LEN=256) :: CLA
   REAL(KIND=REAL128) :: Q(2,6), QD, QT, QC, QS, QE
-  REAL(KIND=REAL32) :: D, T, C, S
-  INTEGER, ALLOCATABLE :: ISEED(:)
-  !DIR$ ATTRIBUTES ALIGN: 64:: ISEED
-  INTEGER :: I, N, NEXP, SSIZE
-  ! random seed may be given
-  CALL RANDOM_SEED(SIZE=SSIZE)
-  IF (SSIZE .LE. 0) STOP 'seed size non-positive'
-  I = COMMAND_ARGUMENT_COUNT()
-  IF (I .LT. 2) THEN
-     IF (SSIZE .GT. 1) THEN
-        WRITE (CLA,'(I1)') SSIZE
-        CLA = 'args: N EXP [SEED1 ... SEED'//TRIM(CLA)//']'
-     ELSE ! SSIZE = 1
-        CLA = 'args: N EXP [SEED1]'
-     END IF
-     WRITE (ERROR_UNIT,*) TRIM(CLA)
-     STOP 'All SEED arguments have to be given, or none of them.'
-  END IF
-  CALL GET_COMMAND_ARGUMENT(2, CLA)
+  REAL(KIND=REAL32) :: D, T, C, S, U
+  INTEGER :: I, N, NEXP
+  INTEGER(KIND=INT32) :: ID
+  EQUIVALENCE (D,ID)
+  IF (COMMAND_ARGUMENT_COUNT() .NE. 1) STOP 'sth2t.exe EXP'
+  CALL GET_COMMAND_ARGUMENT(1, CLA)
   READ (CLA,*) NEXP
   IF (NEXP .GE. 0) STOP 'EXP must be < 0'
-  CALL GET_COMMAND_ARGUMENT(1, CLA)
-  READ (CLA,*) N
-  IF (I .EQ. 2) THEN
-     ALLOCATE(ISEED(SSIZE))
-     CALL RANDOM_SEED
-     CALL RANDOM_SEED(GET=ISEED)
-  ELSE IF (I .EQ. (SSIZE + 2)) THEN
-     ALLOCATE(ISEED(SSIZE))
-     DO I = 1, SSIZE
-        CALL GET_COMMAND_ARGUMENT(I + 2, CLA)
-        READ (CLA,*) ISEED(I)
-     END DO
-     CALL RANDOM_SEED(PUT=ISEED)
-  ELSE ! a wrong SEED
-     STOP 'invalid number of SEED arguments'
-  END IF
-  DO I = 1, SSIZE-1
-     WRITE (ERROR_UNIT,'(I12)',ADVANCE='NO') ISEED(I)
-  END DO
-  WRITE (ERROR_UNIT,'(I12)') ISEED(SSIZE)
-  ISEED = 0
   Q = QZERO
-  D = ZERO
-  DO I = 1, ABS(N)
-1    CALL RANDOM_NUMBER(D)
-     IF (.NOT. (D .GE. TINY(ZERO))) GOTO 1
-     D = FRACTION(D)
-     SSIZE = NEXP + 1
-     D = SCALE(D, SSIZE)
-     IF (D .GE. CUTOFF) THEN
-        ISEED(1) = ISEED(1) + 1
-        IF (N .LT. 0) GOTO 1
-     END IF
+  IF (NEXP .EQ. -1) THEN
+     U = CUTOFF
+  ELSE ! NEXP < -1
+     U = SCALE(ONE, NEXP + 1)
+  END IF
+  D = SCALE(ONE, NEXP)
+  N = 0
+  DO WHILE (D .LT. U)
      ! "enhanced" formulas
      T = D / (ONE + SQRT(IEEE_FMA(-D, D, ONE)))
      C = CR_RSQRT(IEEE_FMA(-T, T, ONE))
@@ -110,15 +73,14 @@ PROGRAM STH2T
      QE = ABS(QS - QE) / QS
      Q(ARE,SSH) = IEEE_FMA(Q(ARE,SSH), QD, QE / I)
      Q(MRE,SSH) = MAX(Q(MRE,SSH), QE)
+     ! increment
+     ID = ID + 1
+     N = N + 1
   END DO
-#ifndef NDEBUG
-  IF (N .LT. 0) WRITE (ERROR_UNIT,'(A,I11)') 'Skipped:', ISEED(1)
-#endif
-  DEALLOCATE(ISEED)
   ! relative errors in the terms of \epsilon
   QE = DEPS
   Q = Q / QE
-  WRITE (OUTPUT_UNIT,'(I3,A,I11,12(A,ES16.9E2))') NEXP, ',', ABS(N), ',', &
+  WRITE (OUTPUT_UNIT,'(I3,A,I11,12(A,ES16.9E2))') NEXP, ',', N, ',', &
        Q(ARE,ETH), ',', Q(MRE,ETH), ',', Q(ARE,ECH), ',', Q(MRE,ECH), ',', Q(ARE,ESH), ',', Q(MRE,ESH), ',', &
        Q(ARE,STH), ',', Q(MRE,STH), ',', Q(ARE,SCH), ',', Q(MRE,SCH), ',', Q(ARE,SSH), ',', Q(MRE,SSH)
 END PROGRAM STH2T
