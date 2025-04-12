c311336d35f4b990e92fe53eb8963389829ae52f
Vedran Novakovic <venovako@venovako.eu>
2025-04-12 17:41:59 +0200

    further updates

---
+++ var/ppjjac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for T in d z
+do
+	for S in 1 3
+	do
+		echo '"N", "J", "GS", "SWEEPS", "TIMEs", "MAXREL", "||G-UΣV^-1||_F/||G||_F"' > $T-$S.csv
+		for ((I=1;I<=$1;I*=2))
+		do
+			let "J=I/2"
+			printf "%3d, %3d," $I $J >> $T-$S.csv
+			cat $T$I-$S.out >> $T-$S.csv
+		done
+	done
+done
+unset S T J I
