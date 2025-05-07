e3877abe46baa197d8ffe23256731cf99289e9ec
Vedran Novakovic <venovako@venovako.eu>
2025-05-07 04:17:32 +0200

    further updates

---
+++ src/gljau2.f90
@@ -0,0 +1,16 @@
+  ES = INT(IAND(INFO, 1), c_int)
+  RT = LJU2(A11, A22, A21, CS, SN, L1, L2, ES)
+  IF (RT .LT. 0_c_int) THEN
+     INFO = INT(RT)
+  ELSE IF ((CS .EQ. ONE) .AND. (SN .EQ. ZERO)) THEN
+     INFO = IOR(INFO, 4)
+  ELSE ! not identity
+     S = INT(ES)
+     IF (S .NE. 0) THEN
+        A11 = SCALE(L1, S)
+        A22 = SCALE(L2, S)
+     ELSE ! S = 0
+        A11 = L1
+        A22 = L2
+     END IF
+  END IF
---
+++ src/hljav2.f90
@@ -0,0 +1,19 @@
+  IF (INFO .EQ. 0) THEN
+     ES = 3_c_int
+  ELSE ! (ch,th)
+     ES = -3_c_int
+  END IF
+  RT = LJV2(A11, A22, A21R, A21I, CH, SHR, SHI, ES)
+  IF (RT .LT. 0_c_int) THEN
+     INFO = INT(RT)
+  ELSE ! OK
+     INFO = IAND(INT(RT), 3)
+     A = CR_HYPOT(SHR, SHI)
+     IF ((.NOT. (A .LE. HUGE(A))) .OR. ((CH .EQ. ONE) .AND. (A .EQ. ZERO))) THEN
+        ! identity or |TH| >= 1 => skip the transformation
+        CH = ONE
+        SHR = ZERO
+        SHI = ZERO
+        INFO = IOR(INFO, 4)
+     END IF
+  END IF
