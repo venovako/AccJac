61ae9208a1b616b1869c0d436655e2078bef5c16
Vedran Novakovic <venovako@venovako.eu>
2025-05-12 00:51:08 +0200

    further updates

---
+++ src/zpproc.F90
@@ -0,0 +1,122 @@
+PROGRAM ZPPROC
+#ifdef __GFORTRAN__
+  USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: OUTPUT_UNIT, REAL64
+#else
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: OUTPUT_UNIT, REAL64, REAL128
+#endif
+  IMPLICIT NONE
+#ifdef __GFORTRAN__
+  INTERFACE
+     PURE FUNCTION HYPOTX(X, Y) BIND(C,NAME='cr_hypotl')
+       USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
+       IMPLICIT NONE
+       REAL(KIND=c_long_double), INTENT(IN), VALUE :: X, Y
+       REAL(KIND=c_long_double) :: HYPOTX
+     END FUNCTION HYPOTX
+  END INTERFACE
+  INTEGER, PARAMETER :: KK = c_long_double
+#else
+#define HYPOTX HYPOT
+  INTEGER, PARAMETER :: KK = REAL128
+#endif
+  INTEGER, PARAMETER :: K = REAL64
+  REAL(KIND=KK), PARAMETER :: XZERO = 0.0_KK
+  REAL(KIND=K), PARAMETER :: ZERO = 0.0_K
+  CHARACTER(LEN=11) :: FN
+  COMPLEX(KIND=KK) :: Z
+  REAL(KIND=KK) :: X, Y
+  INTEGER :: I, J, L, N, S, U, V
+  COMPLEX(KIND=K), ALLOCATABLE :: W1(:,:), A1(:,:), W3(:,:), A3(:,:)
+  REAL(KIND=K), EXTERNAL :: ZNRMF
+  ! read the command line arguments
+  L = COMMAND_ARGUMENT_COUNT()
+  IF (L .NE. 2) STOP 'zpproc.exe N S'
+  CALL GET_COMMAND_ARGUMENT(1, FN)
+  READ (FN,*) N
+  IF (N .LE. 0) STOP 'N'
+  CALL GET_COMMAND_ARGUMENT(2, FN)
+  READ (FN,*) S
+  IF (S .LE. 0) STOP 'S'
+  ALLOCATE(W1(N,N))
+  ALLOCATE(A1(N,N))
+  ALLOCATE(W3(N,N))
+  ALLOCATE(A3(N,N))
+  WRITE (OUTPUT_UNIT,'(A)') '"SWEEP", "P", "Q", "maxRE(tan[h])@PQ", "maxRE(A)"'
+  DO L = 1, S
+     WRITE (FN,'(A,I3.3,A,I2.2,A)') 'z', N, '_', L, '.txt'
+     OPEN(NEWUNIT=U, IOSTAT=J, FILE=FN, STATUS='OLD', ACTION='READ', ACCESS='SEQUENTIAL', FORM='FORMATTED')
+     IF (J .NE. 0) STOP 'OPEN(1)'
+     DO I = 1, N
+        READ (U,*) (W1(I,J), J=1,N)
+     END DO
+     CLOSE(UNIT=U, IOSTAT=J)
+     IF (J .NE. 0) STOP 'CLOSE(1)'
+     DO J = 1, N
+        DO I = 1, J-1
+           A1(I,J) = W1(I,J)
+           W1(I,J) = ZERO
+        END DO
+        A1(J,J) = REAL(W1(J,J))
+        W1(J,J) = ZERO
+     END DO
+     DO J = 1, N-1
+        DO I = J+1, N
+           A1(I,J) = CONJG(A1(J,I))
+        END DO
+     END DO
+     WRITE (FN,'(A,I3.3,A,I2.2,A)') 'z', N, '-', L, '.txt'
+     OPEN(NEWUNIT=U, IOSTAT=J, FILE=FN, STATUS='OLD', ACTION='READ', ACCESS='SEQUENTIAL', FORM='FORMATTED')
+     IF (J .NE. 0) STOP 'OPEN(3)'
+     DO I = 1, N
+        READ (U,*) (W3(I,J), J=1,N)
+     END DO
+     CLOSE(UNIT=U, IOSTAT=J)
+     IF (J .NE. 0) STOP 'CLOSE(3)'
+     DO J = 1, N
+        DO I = 1, J-1
+           A3(I,J) = W3(I,J)
+           W3(I,J) = ZERO
+        END DO
+        A3(J,J) = REAL(W3(J,J))
+        W3(J,J) = ZERO
+     END DO
+     DO J = 1, N-1
+        DO I = J+1, N
+           A3(I,J) = CONJG(A3(J,I))
+        END DO
+     END DO
+     X = XZERO
+     U = 0
+     V = 0
+     DO J = 1, N-1
+        DO I = J+1, N
+           Z = CMPLX(REAL(W1(I,J)), AIMAG(W1(I,J)), KK) - CMPLX(REAL(W3(I,J)), AIMAG(W3(I,J)), KK)
+           Y = HYPOTX(REAL(Z), AIMAG(Z))
+           IF (Y .NE. XZERO) Y = Y / HYPOTX(REAL(REAL(W1(I,J)), KK), REAL(AIMAG(W1(I,J)), KK))
+           IF (Y .GT. X) THEN
+              X = Y
+              U = J
+              V = I
+           END IF
+        END DO
+     END DO
+     WRITE (OUTPUT_UNIT,'(I2,A,2(I4,A),ES25.17E3,A)',ADVANCE='NO') L, ',', U, ',', V, ',', X, ','
+     FLUSH(OUTPUT_UNIT)
+     X = XZERO
+     DO J = 1, N
+        DO I = 1, N
+           Z = CMPLX(REAL(A1(I,J)), AIMAG(A1(I,J)), KK) - CMPLX(REAL(A3(I,J)), AIMAG(A3(I,J)), KK)
+           Y = HYPOTX(REAL(Z), AIMAG(Z))
+           X = HYPOTX(X, Y)
+        END DO
+     END DO
+     V = N * N
+     IF (X .NE. XZERO) X = X / ZNRMF(V, A1)
+     WRITE (OUTPUT_UNIT,'(ES25.17E3)') X
+  END DO
+  DEALLOCATE(A3)
+  DEALLOCATE(W3)
+  DEALLOCATE(A1)
+  DEALLOCATE(W1)
+END PROGRAM ZPPROC
