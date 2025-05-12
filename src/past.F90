249b5b8f7037172f488566c1d63c6a92af6b7745
Vedran Novakovic <venovako@venovako.eu>
2025-05-13 00:48:31 +0200

    further updates

--- src/past.F90
+++ src/past.F90
@@ -2,12 +2,21 @@ PROGRAM PAST
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
   USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: INT64, OUTPUT_UNIT, REAL64
   IMPLICIT NONE
+  INTERFACE
+     PURE SUBROUTINE CR_SINCOS(X, S, C) BIND(C,NAME='cr_sincos')
+       USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_double
+       IMPLICIT NONE
+       REAL(KIND=c_double), INTENT(IN), VALUE :: X
+       REAL(KIND=c_double), INTENT(OUT) :: S, C
+     END SUBROUTINE CR_SINCOS
+  END INTERFACE
+  REAL(KIND=REAL64), PARAMETER :: DPI = 3.14159265358979323846_REAL64
   CHARACTER(LEN=256) :: CLA
   INTEGER(KIND=INT64) :: K, M
-  INTEGER :: N, I, J
+  INTEGER :: N, I, J, R
   LOGICAL :: C
   INTEGER(KIND=INT64), ALLOCATABLE :: L(:,:), U(:,:), S(:,:)
-  REAL(KIND=REAL64), ALLOCATABLE :: A(:,:)
+  REAL(KIND=REAL64), ALLOCATABLE :: A(:,:), RU(:,:)
   REAL(KIND=c_long_double), ALLOCATABLE :: AA(:,:)
   COMPLEX(KIND=REAL64), ALLOCATABLE :: Z(:,:)
   COMPLEX(KIND=c_long_double), ALLOCATABLE :: ZZ(:,:)
@@ -166,15 +175,30 @@ PROGRAM PAST
         WRITE (OUTPUT_UNIT,'(I20)') S(I,N)
      END DO
   END IF
+  IF (C) THEN
+     ! roots of unity
+     ALLOCATE(RU(3,N))
+     DO J = 1, N
+        RU(1,J) = ((2 * (J - 1)) * DPI) / N
+        CALL CR_SINCOS(RU(1,J), RU(3,J), RU(2,J))
+     END DO
+  END IF
   M = ISHFT(1_INT64, 53)
   IF (K .LT. M) THEN
      IF (C) THEN
         ALLOCATE(Z(N,N))
-        DO J = 1, N
-           DO I = 1, N
-              Z(I,J) = L(I,J)
+        DO J = 1, N-1
+           Z(J,J) = L(J,J)
+           DO I = J+1, N
+              Z(I,J) = CMPLX(RU(2,I), RU(3,I), REAL64) * L(I,J)
            END DO
         END DO
+        DO J = 2, N
+           DO I = 1, J-1
+              Z(I,J) = 0.0_REAL64
+           END DO
+        END DO
+        Z(N,N) = L(N,N)
      ELSE ! real
         ALLOCATE(A(N,N))
         DO J = 1, N
@@ -186,11 +210,18 @@ PROGRAM PAST
   ELSE ! K >= M
      IF (C) THEN
         ALLOCATE(ZZ(N,N))
-        DO J = 1, N
-           DO I = 1, N
-              ZZ(I,J) = L(I,J)
+        DO J = 1, N-1
+           ZZ(J,J) = L(J,J)
+           DO I = J+1, N
+              ZZ(I,J) = CMPLX(RU(2,I), RU(3,I), REAL64) * REAL(L(I,J), c_long_double)
            END DO
         END DO
+        DO J = 2, N
+           DO I = 1, J-1
+              ZZ(I,J) = 0.0_c_long_double
+           END DO
+        END DO
+        ZZ(N,N) = L(N,N)
      ELSE ! real
         ALLOCATE(AA(N,N))
         DO J = 1, N
@@ -217,13 +248,22 @@ PROGRAM PAST
   END IF
   IF (J .NE. 0) STOP 'WRITE(Y)'
   CLOSE(I)
+  ! e^{i(2(I-1)π/N)} * e^{-i(2(J-1)π/N)} = e^{i(2(I-J)π/N)}}
   IF (K .LT. M) THEN
      IF (C) THEN
-        DO J = 1, N
-           DO I = 1, N
-              Z(I,J) = S(I,J)
+        DO J = 1, N-1
+           Z(J,J) = S(J,J)
+           DO I = J+1, N
+              R = I - J + 1
+              Z(I,J) = CMPLX(RU(2,R), RU(3,R), REAL64) * S(I,J)
            END DO
         END DO
+        DO J = 2, N
+           DO I = 1, J-1
+              Z(I,J) = CONJG(Z(J,I))
+           END DO
+        END DO
+        Z(N,N) = S(N,N)
      ELSE ! real
         DO J = 1, N
            DO I = 1, N
@@ -233,11 +273,19 @@ PROGRAM PAST
      END IF
   ELSE ! K >= M
      IF (C) THEN
-        DO J = 1, N
-           DO I = 1, N
-              ZZ(I,J) = S(I,J)
+        DO J = 1, N-1
+           ZZ(J,J) = S(J,J)
+           DO I = J+1, N
+              R = I - J + 1
+              ZZ(I,J) = CMPLX(RU(2,R), RU(3,R), REAL64) * REAL(S(I,J), c_long_double)
            END DO
         END DO
+        DO J = 2, N
+           DO I = 1, J-1
+              ZZ(I,J) = CONJG(ZZ(J,I))
+           END DO
+        END DO
+        ZZ(N,N) = S(N,N)
      ELSE ! real
         DO J = 1, N
            DO I = 1, N
@@ -267,6 +315,7 @@ PROGRAM PAST
   END IF
   IF (J .NE. 0) STOP 'WRITE(A)'
   CLOSE(I)
+  IF (C) DEALLOCATE(RU)
   DEALLOCATE(S)
   DEALLOCATE(U)
   DEALLOCATE(L)
