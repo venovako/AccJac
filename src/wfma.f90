47664d18acfca2283e4abad9a05515f7840dd757
Vedran Novakovic <venovako@venovako.eu>
2025-05-15 02:15:16 +0200

    further updates

--- src/wfma.f90
+++ src/wfma.f90
@@ -1,17 +1,20 @@
 PURE FUNCTION WFMA(A, B, C)
+  ! USE, INTRINSIC :: IEEE_ARITHMETIC, ONLY: IEEE_FMA
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
+  ! USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL128
   IMPLICIT NONE
-  INTERFACE
-     PURE SUBROUTINE PVN_WFMA(DR, DI, AR, AI, BR, BI, CR, CI)
-       USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
-       IMPLICIT NONE
-       REAL(KIND=c_long_double), INTENT(OUT) :: DR, DI
-       REAL(KIND=c_long_double), INTENT(IN) :: AR, AI, BR, BI, CR, CI
-     END SUBROUTINE PVN_WFMA
-  END INTERFACE
   COMPLEX(KIND=c_long_double), INTENT(IN) :: A, B, C
   COMPLEX(KIND=c_long_double) :: WFMA
-  REAL(KIND=c_long_double) :: DR, DI
-  CALL PVN_WFMA(DR, DI, REAL(A), AIMAG(A), REAL(B), AIMAG(B), REAL(C), AIMAG(C))
-  WFMA = CMPLX(DR, DI, c_long_double)
+  ! REAL(KIND=REAL128) :: AR, AI, BR, BI, CR, CI
+  ! REAL(KIND=c_long_double) :: DR, DI
+  ! AR = REAL(A)
+  ! AI = AIMAG(A)
+  ! BR = REAL(B)
+  ! BI = AIMAG(B)
+  ! CR = REAL(C)
+  ! CI = AIMAG(C)
+  ! DR = REAL(IEEE_FMA(AR, BR, IEEE_FMA(-AI, BI, CR)), c_long_double)
+  ! DI = REAL(IEEE_FMA(AR, BI, IEEE_FMA( AI, BR, CI)), c_long_double)
+  ! WFMA = CMPLX(DR, DI, c_long_double)
+  WFMA = A * B + C
 END FUNCTION WFMA
