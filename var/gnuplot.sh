ab21bff99be85beb8cb0822d4ba6f52b672f4e12
Vedran Novakovic <venovako@venovako.eu>
2025-05-31 05:15:46 +0200

    further updates

---
+++ var/gnuplot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./prepare
+./configure --enable-dependency-tracking --enable-stable-sort --enable-polar-grid --enable-watchpoints --enable-function-blocks --enable-year2038 --with-bitmap-terminals --with-x --with-gpic --with-tgif --with-metafont --with-metapost --with-regis --with-qt=no --with-readline=builtin --with-amos=/usr/local
