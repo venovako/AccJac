735519f120cfa5e5962d959ebd78ad443ab6a9c2
Vedran Novakovic <venovako@venovako.eu>
2025-06-05 01:41:09 +0200

    further updates

--- src/gscalg.f90
+++ src/gscalg.f90
@@ -1,27 +1,33 @@
+  IF (INFO .LT. 0) INFO = -(INFO + 1)
+#ifndef NDEBUG
   IF (LDG .LT. M) INFO = -4
-  IF ((N .LT. 0) .OR. (N .GT. M)) INFO = -2
+  IF (N .LT. 0) INFO = -2
   IF (M .LT. 0) INFO = -1
   IF (INFO .LT. 0) RETURN
+#endif
   IF (N .EQ. 0) RETURN
   IF (INFO .EQ. 0) THEN
      GX = ZERO
      DO J = 1, N
         DO I = 1, M
            X = ABS(G(I,J))
+#ifndef NDEBUG
            IF (.NOT. (X .LE. HUGE(X))) THEN
               INFO = -3
               RETURN
            END IF
+#endif
            GX = MAX(GX, X)
         END DO
      END DO
   END IF
   ! upper bound
+  S = 3
   IF (M .EQ. 1) THEN
      X = HUGE(X)
   ELSE ! M > 1
      X = M
-     X = X * 3
+     X = X * S
      X = HUGE(X) / X
      X = SCALE(HALF, EXPONENT(X))
   END IF
--- src/hscalg.f90
+++ src/hscalg.f90
@@ -1,27 +1,54 @@
+  ! INFO < 0 => GX is MAX(ABS(RE(g)),ABS(IM(g)))
+  ! INFO >= 0 => GX is MAX(ABS(g))
+  IF (INFO .LT. 0) THEN
+     INFO = -(INFO + 1)
+     S = 5
+  ELSE ! INFO .GE. 0
+     S = 3
+  END IF
+#ifndef NDEBUG
   IF (LDG .LT. M) INFO = -4
-  IF ((N .LT. 0) .OR. (N .GT. M)) INFO = -2
+  IF (N .LT. 0) INFO = -2
   IF (M .LT. 0) INFO = -1
   IF (INFO .LT. 0) RETURN
+#endif
   IF (N .EQ. 0) RETURN
   IF (INFO .EQ. 0) THEN
      GX = ZERO
-     DO J = 1, N
-        DO I = 1, M
-           X = CR_HYPOT(REAL(G(I,J)), AIMAG(G(I,J)))
-           IF (.NOT. (X .LE. HUGE(X))) THEN
-              INFO = -3
-              RETURN
-           END IF
-           GX = MAX(GX, X)
+     IF (S .EQ. 5) THEN
+        DO J = 1, N
+           DO I = 1, M
+              X = MAX(ABS(REAL(G(I,J))), ABS(AIMAG(G(I,J))))
+#ifndef NDEBUG
+              IF (.NOT. (X .LE. HUGE(X))) THEN
+                 INFO = -3
+                 RETURN
+              END IF
+#endif
+              GX = MAX(GX, X)
+           END DO
         END DO
-     END DO
+     ELSE ! S = 3
+        DO J = 1, N
+           DO I = 1, M
+              X = CR_HYPOT(REAL(G(I,J)), AIMAG(G(I,J)))
+#ifndef NDEBUG
+              IF (.NOT. (X .LE. HUGE(X))) THEN
+                 INFO = -3
+                 RETURN
+              END IF
+#endif
+              GX = MAX(GX, X)
+           END DO
+        END DO
+     END IF
   END IF
   ! upper bound
   IF (M .EQ. 1) THEN
      X = HUGE(X)
   ELSE ! M > 1
      X = M
-     X = X * 3
+     X = X * S
      X = HUGE(X) / X
      X = SCALE(HALF, EXPONENT(X))
   END IF
