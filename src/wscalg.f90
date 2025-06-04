94eb6ce9c42c79c590ddc04385f53a9eb13b17ae
Vedran Novakovic <venovako@venovako.eu>
2025-06-05 00:26:56 +0200

    further updates

--- src/wscalg.f90
+++ src/wscalg.f90
@@ -1,8 +1,13 @@
 ! the first time this routine is called, let GS = 0 and INFO = 0
 ! otherwise, set INFO > 0 (e.g., to the step number)
 PURE SUBROUTINE WSCALG(M, N, G, LDG, GX, GS, INFO)
+#ifdef __GFORTRAN__
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
+#else
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL128
+#endif
   IMPLICIT NONE
+#ifdef __GFORTRAN__
   INTERFACE
      PURE FUNCTION CR_HYPOT(X, Y) BIND(C,NAME='cr_hypotl')
        USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
@@ -12,6 +17,10 @@ PURE SUBROUTINE WSCALG(M, N, G, LDG, GX, GS, INFO)
      END FUNCTION CR_HYPOT
   END INTERFACE
   INTEGER, PARAMETER :: K = c_long_double
+#else
+#define CR_HYPOT HYPOT
+  INTEGER, PARAMETER :: K = REAL128
+#endif
   REAL(KIND=K), PARAMETER :: ZERO = 0.0_K, HALF = 0.5_K
   INTEGER, INTENT(IN) :: M, N, LDG
   COMPLEX(KIND=K), INTENT(INOUT) :: G(LDG,N)
--- src/xscalg.f90
+++ src/xscalg.f90
@@ -1,9 +1,17 @@
 ! the first time this routine is called, let GS = 0 and INFO = 0
 ! otherwise, set INFO > 0 (e.g., to the step number)
 PURE SUBROUTINE XSCALG(M, N, G, LDG, GX, GS, INFO)
+#ifdef __GFORTRAN__
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_long_double
+#else
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL128
+#endif
   IMPLICIT NONE
+#ifdef __GFORTRAN__
   INTEGER, PARAMETER :: K = c_long_double
+#else
+  INTEGER, PARAMETER :: K = REAL128
+#endif
   REAL(KIND=K), PARAMETER :: ZERO = 0.0_K, HALF = 0.5_K
   INTEGER, INTENT(IN) :: M, N, LDG
   REAL(KIND=K), INTENT(INOUT) :: G(LDG,N), GX
