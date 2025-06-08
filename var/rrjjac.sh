c02643d12666721437a3b000dbb88c656fd73606
Vedran Novakovic <venovako@venovako.eu>
2025-06-09 01:53:13 +0200

    further updates

---
+++ var/rrjjac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for ((I=$1;I<=$2;I*=2))
+do
+	for T in d z
+	do
+		for S in 0 2 4 6
+		do
+			echo $I $T $S
+			cd $S
+			$3/${T}jsvrr.exe $I $I $T$I > $T$I-$S.rr 2>> $T$I-$S.err &
+			cd ..
+		done
+	done
+done
+wait
+unset S T I
