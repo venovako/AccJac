4b99f75dc326f51d7273cbfaad07adb11676d3a5
Vedran Novakovic <venovako@venovako.eu>
2025-06-11 05:51:22 +0200

    further updates

--- var/rrpjjac.sh
+++ var/rrpjjac.sh
@@ -9,6 +9,10 @@ do
 			echo $I $T $S
 			cd $S
 			OMP_NUM_THREADS=$4 $3/src/${T}jsvrr.exe $I $I $T$I > $T$I-$S.rr 2>> $T$I-$S.err
+			echo -n "|| U^H U - I ||_F =" >> $T$I-$S.rr
+			OMP_NUM_THREADS=$4 $3/src/${T}jsvor.exe $I $I $T$I.YU >> $T$I-$S.rr 2>> $T$I-$S.err
+			echo -n "|| V^H J V - J ||_F =" >> $T$I-$S.rr
+			OMP_NUM_THREADS=$4 $3/src/${T}jsvor.exe $I -1 $T$I.YV >> $T$I-$S.rr 2>> $T$I-$S.err
 			cd ..
 		done
 	done
