cf7134b315cf669262f5e6f4c3e40ca2e1070673
Vedran Novakovic <venovako@venovako.eu>
2025-06-15 00:58:21 +0200

    further updates

--- var/offrnd/d512.sh
+++ var/offrnd/d512.sh
@@ -4,7 +4,7 @@ for S in 1 7 9
 do
 	OMP_NUM_THREADS=1 ./djevdt.exe -512  256 $S d512 > d512_$S.out 2> d512_$S.err
 	mv [Dd]512.csv d512_$S.csv
-	OMP_NUM_THREADS=1 ./djevdt.exe -512 -257 $S d512 > d512-$S.out 2> d512-$2.err
+	OMP_NUM_THREADS=1 ./djevdt.exe -512 -257 $S d512 > d512-$S.out 2> d512-$S.err
 	mv [Dd]512.csv d512-$S.csv
 done
 unset S
