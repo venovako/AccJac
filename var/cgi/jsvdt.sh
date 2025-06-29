332b493c534980a6690bda8b9179075f021cbac6
Vedran Novakovic <venovako@venovako.eu>
2025-06-30 01:07:51 +0200

    further updates

--- var/cgi/jsvdt.sh
+++ var/cgi/jsvdt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ "$1" = "" ]
+# Usage: ./jsvdt.sh MARCH [ GNU_SUFFIX ]
+if [ "$2" = "" ]
 then
-	gcc -I../../../cgic -I../../../libpvn/src -O3 -march=native -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -c jsvdt.c
-	gfortran -DUSE_IEEE_INTRINSIC=IEEE_FMA -O3 -march=native -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -ffree-line-length-none -fprotect-parens -frecursive -fstack-arrays jsvdt.F90 jsvdt.o -o jsvdt.cgi -rdynamic -static -s -L../../src -ljaevd -ljaev2 -L../../../cgic -lcgic -L../../../libpvn/src -lpvn -lm
+	gcc -I../../../cgic -I../../../libpvn/src -O3 -march=$1 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -c jsvdt.c
+	gfortran -DUSE_IEEE_INTRINSIC=IEEE_FMA -O3 -march=$1 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -ffree-line-length-none -fprotect-parens -frecursive -fstack-arrays jsvdt.F90 jsvdt.o -o jsvdt.cgi -rdynamic -static -s -L../../src -ljaevd -ljaev2 -L../../../cgic -lcgic -L../../../libpvn/src -lpvn -lm
 else
-	gcc$1 -I../../../cgic -I../../../libpvn/src -O3 -march=native -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -c jsvdt.c
-	gfortran$1 -DUSE_IEEE_INTRINSIC=IEEE_FMA -O3 -march=native -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -ffree-line-length-none -fprotect-parens -frecursive -fstack-arrays jsvdt.F90 jsvdt.o -o jsvdt.cgi -rdynamic -L../../src -ljaevd -ljaev2 -L../../../cgic -lcgic -L../../../libpvn/src -lpvn -lm
+	gcc$2 -I../../../cgic -I../../../libpvn/src -O3 -march=$1 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -c jsvdt.c
+	gfortran$2 -DUSE_IEEE_INTRINSIC=IEEE_FMA -O3 -march=$1 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -ffree-line-length-none -fprotect-parens -frecursive -fstack-arrays jsvdt.F90 jsvdt.o -o jsvdt.cgi -rdynamic -L../../src -ljaevd -ljaev2 -L../../../cgic -lcgic -L../../../libpvn/src -lpvn -lm
 fi
