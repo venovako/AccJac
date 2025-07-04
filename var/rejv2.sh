132f6c4227959876692c4786561896ac9f8fd8a7
Vedran Novakovic <venovako@venovako.eu>
2025-07-04 23:09:31 +0200

    further updates

--- var/rejv2.sh
+++ var/rejv2.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 for p in 24 53 64
 do
+	echo -n "${p} -113 9 ... "
 	./rejv2.wls ${p} -113 9 > rejv2-${p}.txt
+	echo "rejv2-${p}.txt"
+	echo -n "${p}  113 9 ... "
 	./rejv2.wls ${p}  113 9 > rejv2_${p}.txt
+	echo "rejv2_${p}.txt"
 done
 unset p
