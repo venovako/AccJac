464a563f0d2a7ca45266b7975a021a94ca2ca569
Vedran Novakovic <venovako@venovako.eu>
2025-07-04 08:04:47 +0200

    further updates

--- var/ompjjac.sh
+++ var/ompjjac.sh
@@ -4,11 +4,11 @@ for ((I=$1;I<=$2;I*=2))
 do
 	for T in d z
 	do
-		for S in 0 2 4 6
+		for S in 1 2 3 4
 		do
 			echo $I $T $S
 			cd $S
-			OMP_NUM_THREADS=$4 $3/${T}jsvdx.exe $I $5$I -1 $S $T$I > $T$I-$S.out 2> $T$I-$S.err
+			OMP_NUM_THREADS=$4 $3/${T}jsvdx.exe $I $5$I -1 -$S $T$I > $T$I-$S.out 2> $T$I-$S.err
 			cd ..
 		done
 	done
