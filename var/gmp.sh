ef81d12525076ccbcce70e2119b5d0df755fc29a
Vedran Novakovic <venovako@venovako.eu>
2025-09-09 22:06:36 +0200

    further updates

--- var/gmp.sh
+++ var/gmp.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 ./.bootstrap
-#CC=icx CXX=icpx
 ./configure --prefix=/opt/gmp --enable-cxx --with-pic
--- var/mpc.sh
+++ var/mpc.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 autoreconf -f -i -v
-#CC=icx
 ./configure --prefix=/opt/mpc --enable-dependency-tracking --with-mpfr=/opt/mpfr --with-gmp=/opt/gmp --with-pic
