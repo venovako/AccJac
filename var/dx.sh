b246d7eb69abf31ea8cb12b6f4ce41803f2bc98d
Vedran Novakovic <venovako@venovako.eu>
2025-09-10 20:43:06 +0200

    further updates

---
+++ var/dx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+../src/dxresc.exe 256 256 128 -64 1 d256
+../src/xjsvdx.exe 256 256 128 0 d256
+mv -fv d256.SY d256.SX
+mv -fv d256.X d256.Y
+../src/djsvdx.exe 256 256 128 0 d256
+../src/dxsvrr.exe 256 d256 > dx-0.csv 2>&1
+../src/djsvdx.exe 256 256 128 2 d256
+../src/dxsvrr.exe 256 d256 > dx-2.csv 2>&1
