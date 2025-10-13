75589bb97aeac602b51f40732a6e5f0b1b8a2933
Vedran Novakovic <venovako@venovako.eu>
2025-10-13 23:14:46 +0200

    further updates

--- var/cgi/jsvdt.sh
+++ var/cgi/jsvdt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Usage: ./jsvdt.sh [ GNU_SUFFIX ]
 gcc$1 -I../../../cgic -I../../../libpvn/src -O3 -march=native -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -c jsvdt.c
-gfortran$1 -DUSE_IEEE_INTRINSIC=IEEE_FMA -O3 -march=native -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -ffree-line-length-none -fprotect-parens -frecursive -fstack-arrays jsvdt.F90 jsvdt.o -o jsvdt.cgi -rdynamic -s ../../src/libjaevd.a ../../src/libjaev2.a ../../../cgic/libcgic.a ../../../libpvn/src/libpvn.a -ldl -lm
+gfortran$1 -DUSE_IEEE_INTRINSIC=IEEE_FMA -O3 -march=native -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fvect-cost-model=unlimited -ffp-contract=fast -fno-math-errno -ffree-line-length-none -fprotect-parens -frecursive -fstack-arrays jsvdt.F90 jsvdt.o -o jsvdt.cgi -rdynamic -static-libgfortran -static-libquadmath -static-libgcc ../../src/libjaevd.a ../../src/libjaev2.a ../../../cgic/libcgic.a ../../../libpvn/src/libpvn.a -ldl -lm
