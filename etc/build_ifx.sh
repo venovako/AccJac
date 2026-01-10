9570590b73cabb78f444a7414930229eebffcc19
Vedran Novakovic <venovako@venovako.eu>
2026-01-10 23:59:48 +0100

    further updates

--- etc/build_ifx.sh
+++ etc/build_ifx.sh
@@ -5,18 +5,14 @@ then
 	cd ../../libpvn/src
 	make COMPILER=icx NDEBUG=3 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
 	make COMPILER=icx NDEBUG=3 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j all
-	cd ../../cgic
-	make -f Makefile.pvn clean all
-	cd ../AccJac/src
-	make CGIC=../../cgic clean all
+	cd ../../AccJac/src
+	make clean all
 	cd ../etc
 else
 	cd ../../libpvn/src
 	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
 	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j all
-	cd ../../cgic
-	make -f Makefile.pvn clean all
-	cd ../AccJac/src
-	make CGIC=../../cgic clean all
+	cd ../../AccJac/src
+	make clean all
 	cd ../etc
 fi
