2fc3c423e9351dc14afc26e8351c42157968ca5c
Vedran Novakovic <venovako@venovako.eu>
2026-08-30 06:15:26 +0200

    further updates

--- etc/build_ifx.sh
+++ etc/build_ifx.sh
@@ -3,15 +3,15 @@
 if [ -z "${TGT}" ]
 then
 	cd ../../libpvn/src
-	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
-	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j
+	make COMPILER=icx NDEBUG=3 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
+	make COMPILER=icx NDEBUG=3 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j
 	cd ../../AccJac/src
 	make clean all
 	cd ../etc
 else
 	cd ../../libpvn/src
-	make COMPILER=icx NATIVE=haswell MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
-	make COMPILER=icx NATIVE=haswell MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j
+	make COMPILER=icx NATIVE=haswell MARCH=${TGT} NDEBUG=3 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
+	make COMPILER=icx NATIVE=haswell MARCH=${TGT} NDEBUG=3 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j
 	cd ../../AccJac/src
 	make clean all
 	cd ../etc
